213976b3e1b2eff4dc472636e7d69fec6e7f5f10
Maksim <45427901+flydecisive@users.noreply.github.com>
2024-02-23 17:56:31 +0300

    add header

--- apps/infoapp/src/app/App.vue
+++ apps/infoapp/src/app/App.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Main from './Pages/Main/Main.vue';
+</script>
 
 <template>
-  <div></div>
+  <Main></Main>
 </template>
---
+++ apps/infoapp/src/app/Pages/Main/Main.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from '../../Components/Header/Header.vue';
+</script>
+
+<template>
+  <Header></Header>
+</template>
+
+<style scoped>
+div {
+  width: 100px;
+  height: 30px;
+  background-color: #2d2cff;
+}
+</style>
